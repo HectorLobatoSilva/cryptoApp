99ab35d82a90760858dab1f6698f60e5d7e84709
HectorLobatoSilva <lobato_man@hotmail.com>
2022-02-05 20:44:37 -0600

    project

--- client/src/utils/constants.tsx
+++ client/src/utils/constants.tsx
@@ -4,6 +4,5 @@ export const shortAddress = (address: string) => {
 };
 export class Constants {
     static readonly CONTRACT_ABI: Array<any> = abi.abi;
-    static readonly CONTRACT_ADDRESS: any =
-        '0xE28f8EFD635EcFb200f36196Ce9C7409DCea41A5';
+    static readonly CONTRACT_ADDRESS: any = '';
 }
